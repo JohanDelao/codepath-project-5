f4a7862ea756b6521e678628ea53fc8df0d2d092
Johan Delao <johandelao10@gmail.com>
2023-03-27 19:30:38 -0400

    finished

--- pokemon-app/src/App.jsx
+++ pokemon-app/src/App.jsx
@@ -15,6 +15,8 @@ function App() {
     height: 7,
     weight: 69,
   });
+  const [filter, setFilter] = useState(pokemon);
+  const [searchInput, setSearchInput] = useState("")
 
   async function fetchData() {
     for (let i = 1; i < 150; i++) {
@@ -22,16 +24,22 @@ function App() {
       const response = await axios.get(url);
       let pokemonData = response.data;
       let types = pokemonData.types;
+      let stats = pokemonData.stats;
       let name = capitalizeFirstLetter(pokemonData.name);
       let newTypes = types.map((data) => {
         return data.type.name;
       });
+      let key = generateUniqueKey()
       let pokemonObject = {
         name: name,
         types: newTypes,
         image: pokemonData.sprites.other.dream_world.front_default,
+        hp: stats[0].base_stat,
+        attack: stats[1].base_stat,
+        defense: stats[2].base_stat,
         height: pokemonData.height,
         weight: pokemonData.weight,
+        id: key,
       };
       setPokemon((pokemons) => [...pokemons, pokemonObject]);
     }
@@ -41,13 +49,45 @@ function App() {
     fetchData();
   }, []);
 
-  function consolePokemon() {
-    console.log(pokemon);
+  function consolePokemon(key) {
+    let select = findObjectByKey(pokemon, 'id', key);
+    console.log(select.hp)
+    console.log(select.attack)
+    console.log(select.defense)
+    setSelectedPokemon(select);
   }
 
   function capitalizeFirstLetter(word) {
     return word.charAt(0).toUpperCase() + word.slice(1);
   }
+
+  function findObjectByKey(array, key, value) {
+    return array.find(obj => obj[key] === value);
+  }
+
+  function generateUniqueKey() {
+    const keyLength = 10;
+    const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    let key = '';
+  
+    for (let i = 0; i < keyLength; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      key += characters[randomIndex];
+    }
+  
+    return key;
+  }
+
+  const searchItems = searchValue => {
+    setSearchInput(searchValue);
+    if (searchValue !== "") {
+      const filteredData = pokemon.filter(item => item.name.toLowerCase().startsWith(searchValue))
+      setFilter(filteredData);
+    } else {
+      setFilter(pokemon);
+    }
+    console.log(filter)
+  };
 
   return (
     <div className="App">
@@ -57,14 +97,7 @@ function App() {
           <input
             id="searchBar"
             placeholder="Search"
-            onChange={(inputString) => setPokemon(pokemon.forEach((pokemon) => {
-              if(pokemon.includes(inputString)){
-                return true;
-              }else{
-                return false;
-              }
-            }
-            ))}
+            onChange={(inputString) => searchItems(inputString.target.value.toLowerCase())}
           ></input>
         </div>
       </div>
@@ -142,9 +175,16 @@ function App() {
           </div>
         </div>
         <div className="pokemonContainer">
-          {pokemon.map((pokemon) => {
+          {searchInput.length > 0 ? filter.map((pokemon) => {
             return (
-              <div className="pokemon">
+              <div className="pokemon" key={pokemon.id} onClick={() => consolePokemon(pokemon.id)}>
+                <img src={pokemon.image} width={100} height={100}></img>
+                <h3 className="pokemonName">{pokemon.name}</h3>
+              </div>
+            );
+          }) : pokemon.map((pokemon) => {
+            return (
+              <div className="pokemon" key={pokemon.id} onClick={() => consolePokemon(pokemon.id)}>
                 <img src={pokemon.image} width={100} height={100}></img>
                 <h3 className="pokemonName">{pokemon.name}</h3>
               </div>
